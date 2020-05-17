2997389862365ba8029a2d0cf0c6c504f24b3164
Josh2609 <jcorps@gmx.com>
2020-05-17 19:03:24 +0100

    Setup client

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss'
+
+class Car extends React.Component {
+  render() {
+    return <h2>COVID TRAVEL RESTRICTIONS</h2>;
+  }
+}
+ReactDOM.render(
+  <Car />,
+  document.getElementById('root')
+);
+
