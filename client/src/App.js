8eb49d590420f7b8f0607b7da26d269cc694b19e
Josh2609 <jcorps@gmx.com>
2020-05-22 18:47:39 +0100

    Refactored CountryList into separate component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,12 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import {Switch} from 'react-router';
-import Country from './Country';
-
+import CountryList from './CountryList';
 
 export default class App extends React.Component {
-  state = {
-    countries: [],
-    persons: []
-  }
-
-  componentDidMount() {
-    axios.get(`http://192.168.1.225:5000/api/country`)
-      .then(res => {
-        const countries = res.data.countries;
-        console.log(countries)
-        this.setState({ countries });
-      })
-      .catch((err) => console.log(err));
-  }
 
   render() {
-    const location = this.props.location
     return (
-      <HashRouter>
-        <div>
-          <input type="text" name="search"></input>
-          <table>
-            <thead>
-              <tr>
-                <th>Country</th>
-                <th>Entry Restrictions</th>
-                <th>Exit Restrictions</th>
-                <th>Transit Restrictions</th>
-                <th>Quarantine Required</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                this.state.countries.map((country, i) => {
-                  var route = "/country/" + country.name
-                  return (
-                    <tr key={i}>
-                      <th><NavLink to={route} >{country.name}</NavLink></th>
-                      <th>{country.entryRestrictions ? "Yes" : "No"}</th>
-                      <th>{country.exitRestrictions ? "Yes" : "No"}</th>
-                      <th>{country.transitRestrictions ? "Yes" : "No"}</th>
-                      <th>{country.quarantineRequired ? "Yes" : "No"}</th>
-                    </tr>
-                  );
-
-                })
-              }
-            </tbody>
-          </table>
-          
-          <div className="content">
-
-            <Route exact path="/country/:countryName" component={Country} />
-
-          </div>
-        </div>
-      </HashRouter>
+      <CountryList />
     )
   }
 }
-
-//export default App;
---
+++ client/src/CountryList.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
+  import Country from './Country';
+
+
+class CountryList extends Component {
+
+    state = {
+        countries: [],
+        persons: [],
+      }
+      
+    componentDidMount() {
+        axios.get(`http://192.168.1.225:5000/api/country`)
+            .then(res => {
+                const countries = res.data.countries;
+                console.log(countries)
+                this.setState({ countries });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <input type="text" name="search"></input>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Country</th>
+                                <th>Entry Restrictions</th>
+                                <th>Exit Restrictions</th>
+                                <th>Transit Restrictions</th>
+                                <th>Quarantine Required</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.countries.map((country, i) => {
+                                    var route = "/country/" + country.name
+                                    return (
+                                        <tr key={i}>
+                                            <th><NavLink to={route} >{country.name}</NavLink></th>
+                                            <th>{country.entryRestrictions ? "Yes" : "No"}</th>
+                                            <th>{country.exitRestrictions ? "Yes" : "No"}</th>
+                                            <th>{country.transitRestrictions ? "Yes" : "No"}</th>
+                                            <th>{country.quarantineRequired ? "Yes" : "No"}</th>
+                                        </tr>
+                                    );
+
+                                })
+                            }
+                        </tbody>
+                    </table>
+
+                    <div className="content">
+
+                        <Route exact path="/country/:countryName" component={Country} />
+
+                    </div>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default CountryList;
