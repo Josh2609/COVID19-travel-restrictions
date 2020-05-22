db04a9188227a3080ad165407f8f082aad965a84
Josh2609 <jcorps@gmx.com>
2020-05-22 11:09:51 +0100

    Added country info tto page with links to extended info

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import {Switch} from 'react-router';
+import Country from './Country';
 
 
 export default class App extends React.Component {
@@ -10,45 +17,57 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`http://localhost:5000/api/country`)
+    axios.get(`http://192.168.1.225:5000/api/country`)
       .then(res => {
         const countries = res.data.countries;
         console.log(countries)
         this.setState({ countries });
       })
+      .catch((err) => console.log(err));
   }
 
   render() {
+    const location = this.props.location
     return (
-      <div>
-        <input type="text" name="search"></input>
-        <table>
-          <thead>
-            <tr>
-              <th>Country</th>
-              <th>Entry Restrictions</th>
-              <th>Exit Restrictions</th>
-              <th>Transit Restrictions</th>
-              <th>Quarantine Required</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.countries.map((country, i) => {
-                return (
-                  <tr key={i}>
-                    <th>{country.name}</th>
-                    <th>{country.entryRestrictions ? "Yes" : "No"}</th>
-                    <th>{country.exitRestrictions ? "Yes" : "No"}</th>
-                    <th>{country.transitRestrictions ? "Yes" : "No"}</th>
-                    <th>{country.quarantineRequired ? "Yes" : "No"}</th>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        </table>
-      </div>
+      <HashRouter>
+        <div>
+          <input type="text" name="search"></input>
+          <table>
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Entry Restrictions</th>
+                <th>Exit Restrictions</th>
+                <th>Transit Restrictions</th>
+                <th>Quarantine Required</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.countries.map((country, i) => {
+                  var route = "/country/" + country.name
+                  return (
+                    <tr key={i}>
+                      <th><NavLink to={route} >{country.name}</NavLink></th>
+                      <th>{country.entryRestrictions ? "Yes" : "No"}</th>
+                      <th>{country.exitRestrictions ? "Yes" : "No"}</th>
+                      <th>{country.transitRestrictions ? "Yes" : "No"}</th>
+                      <th>{country.quarantineRequired ? "Yes" : "No"}</th>
+                    </tr>
+                  );
+
+                })
+              }
+            </tbody>
+          </table>
+          
+          <div className="content">
+
+            <Route exact path="/country/:countryName" component={Country} />
+
+          </div>
+        </div>
+      </HashRouter>
     )
   }
 }
--- server/routes/country.js
+++ server/routes/country.js
@@ -5,9 +5,19 @@ const { Country } = require("../models/Country");
 router.get("/", (req, res) => {
     Country.find().exec((err, countries) => {
         if (err) return res.status(400).json({ success: false, err })
+        console.log(countries)
         res.status(200).json({success: true, countries, postSize: countries.length })
     })
 
+})
+
+router.get("/:countryName", (req, res) => {
+    console.log(req.params.countryName)
+    Country.findOne({name : req.params.countryName}).exec((err, countryData) => {
+        if (err) return res.status(400).json({ success: false, err })
+        console.log(countryData)
+        res.status(200).json({success: true, countryData,})
+    })
 })
 
 module.exports = router;
