568c40a38ad98a0291744b6cbb83819b9653e1eb
Josh2609 <jcorps@gmx.com>
2020-06-10 20:39:23 +0100

    Added sortable tables

--- client/src/CountryList.js
+++ client/src/CountryList.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from 'axios';
 import { NavLink } from "react-router-dom";
 import Table from 'react-bootstrap/Table'
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 
 class CountryList extends Component {
@@ -19,10 +21,41 @@ class CountryList extends Component {
       })
       .catch((err) => console.log(err));
   }
-
+  
   render() {
+    const data = []
+    this.state.countries.map((country, i) => {
+      var route = "/country/" + country.linkName
+      data[i] = 
+        { 
+          name: <NavLink to={route} >{country.name}</NavLink>,
+          entry: country.restrictions.entry ? "Yes" : "No",
+          transit: country.restrictions.transit ? "Yes" : "No",
+          quarantine: country.restrictions.quarantine ? "Yes" : "No"
+        }
+    })
+    const columns = [{
+      dataField: 'name',
+      text: 'Country',
+      sort: true
+    }, {
+      dataField: 'entry',
+      text: 'Entry Restrictions',
+      sort: true
+    }, {
+      dataField: 'transit',
+      text: 'Transit Restrictions',
+      sort: true
+    }, {
+      dataField: 'quarantine',
+      text: 'Quarantine Required',
+      sort: true
+    }];
+    
+    
     return (
       <div>
+        <BootstrapTable striped bordered hover responsive keyField='id' data={ data } columns={ columns } />
         <Table striped bordered hover responsive>
           <thead>
             <tr>
